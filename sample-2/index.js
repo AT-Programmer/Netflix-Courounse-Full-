a3049fddd9139e870c8950bcb8b23112d94265be
AT Programmer <kakkukmr50@gmail.com>
2020-09-29 00:43:04 +0530

    Add Select Image by dots functionality

--- sample-2/index.js
+++ sample-2/index.js
@@ -1,17 +1,25 @@
+var imageSelectors = document.querySelectorAll('.imageSelector')
+var slides = document.getElementsByClassName("slider-img");
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides((slideIndex += n));
+
 }
 
 function currentSlide(n) {
+  console.log(imageSelectors)
+
   showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
+
+
   var i;
-  var slides = document.getElementsByClassName("slider-img");
+
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -21,5 +29,9 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  
+  imageSelectors[slideIndex - 1].checked = true
   slides[slideIndex - 1].style.display = "block";
 }
+
+
